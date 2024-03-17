eb288e2f2cb60e885e0d3e060debf8aab6eecbd2
中野駿介 <nakanoshunsuke@nakanoshunsukenoMacBook-Air.local>
2024-03-17 19:18:09 +0900

    セクション5完了

--- react-guide-material/05_state_and_event/src/060_state_object/start/Example.js
+++ react-guide-material/05_state_and_event/src/060_state_object/start/Example.js
@@ -2,6 +2,32 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState(personObj);
+
+  const changeName = (e) => {
+     person.name = '';
+    console.log({...person} === person);
+    console.log({...person, name: e.target.value, age: 20});
+    // setPerson({name: e.target.value, age: person.age})
+    setPerson({...person, name: e.target.value})
+  }
+  const changeAge = (e) => {
+    setPerson({name: person.name, age: e.target.value})
+  }
+  const reset = () => {
+    setPerson({name: '', age: ''})
+  }
+  return (
+    <>
+      <h3>Name: {person.name}</h3>
+      <h3>Age: {person.age}</h3>
+      <input type="text" value={person.name} onChange={changeName}></input>
+      <input type="number" value={person.age} onChange={changeAge}></input>
+      <div>
+        <button onClick={reset}>リセット</button>
+      </div>
+    </>
+  )
 };
 
 export default Example;
