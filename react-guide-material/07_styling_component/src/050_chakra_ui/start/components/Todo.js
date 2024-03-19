b1b3e12abe9af1d233f861bc20a8c45b31b6abee
中野駿介 <nakanoshunsuke@nakanoshunsukenoMacBook-Air.local>
2024-03-19 13:17:28 +0900

    セクション7

--- react-guide-material/07_styling_component/src/050_chakra_ui/start/components/Todo.js
+++ react-guide-material/07_styling_component/src/050_chakra_ui/start/components/Todo.js
@@ -1,6 +1,9 @@
-import { useState } from "react"
-import List from "./List"
-import Form from "./Form"
+import { useState } from "react";
+// POINT Chakra UIのホームページから使用したいコンポーネントを見つけてインポート
+import { VStack, Heading } from "@chakra-ui/react";
+
+import List from "./List";
+import Form from "./Form";
 
 const Todo = () => {
   const todosList = [
@@ -18,7 +21,7 @@ const Todo = () => {
     },
   ];
 
-  const [ todos, setTodos ] = useState(todosList);
+  const [todos, setTodos] = useState(todosList);
 
   const deleteTodo = (id) => {
     const newTodos = todos.filter((todo) => {
@@ -26,17 +29,20 @@ const Todo = () => {
     });
 
     setTodos(newTodos);
-  }
+  };
 
   const createTodo = (todo) => {
     setTodos([...todos, todo]);
-  }
+  };
 
   return (
-    <>
-      <List todos={todos} deleteTodo={deleteTodo}/>
-      <Form createTodo={createTodo}/>
-    </>
-  )
+    <VStack p="10" spacing="10">
+      <Heading color="blue.200" fontSize="5xl">
+        Reminder
+      </Heading>
+      <List todos={todos} deleteTodo={deleteTodo} />
+      <Form createTodo={createTodo} />
+    </VStack>
+  );
 };
 export default Todo;
